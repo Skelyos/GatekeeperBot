edc1217e1ea6c8a6a3de7104cb50397b6cafe89a
Skelyos <pat123john123@gmail.com>
2018-02-19 23:07:30 +0000

    Add CRUD commands
    View command is pretty crap though

--- GatekeeperBot/MyCommands.cs
+++ GatekeeperBot/MyCommands.cs
@@ -13,16 +13,85 @@ namespace GatekeeperBot
 {
     public class MyCommands
     {
-        [Command("AddToBlacklist")]
-        [Aliases("addtoblacklist", "blacklist", "Blacklist", "BlackList")]
-        public async Task AddToBlacklist(CommandContext ctx)
+        string Blacklist = System.Configuration.ConfigurationManager.AppSettings["Blacklist"];
+        string BlacklistedGuild = System.Configuration.ConfigurationManager.AppSettings["BlacklistedGuild"];
+
+        [Group("Admin")]
+        [RequirePermissions(Permissions.ManageMessages)]
+        public class CRUD
         {
+            string Blacklist = System.Configuration.ConfigurationManager.AppSettings["Blacklist"];
             string BlacklistedGuild = System.Configuration.ConfigurationManager.AppSettings["BlacklistedGuild"];
-            using (StreamWriter file = new StreamWriter(BlacklistedGuild, true))
+
+            public void UserUpdate(CommandContext f, string remove)
             {
-                file.WriteLine(ctx.Guild.Id + "," + ctx.Guild.Name);
+                List<String> lines = new List<String>();
+                using (StreamReader reader = new StreamReader(Blacklist + "-" + f.Guild.Id))
+                {
+                    String line;
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (line.Contains(remove))
+                        {
+                            line = "";
+                        }
+                        lines.Add(line);
+                    }
+                }
+
+                using (StreamWriter writer = new StreamWriter(Blacklist + "-" + f.Guild.Id, false))
+                {
+                    foreach (String line in lines)
+                        writer.WriteLine(line);
+                }
             }
-            await ctx.Message.RespondAsync("Your guild has been added to the blacklist");
+
+            [Command("AddWord")]
+            [Aliases("addWord", "add", "Add", "Word", "word")]
+            public async Task AddWord(CommandContext ctx, string BlackListedWord)
+            {
+                using (StreamWriter file = new StreamWriter(Blacklist + "-" + ctx.Guild.Id, true))
+                {
+                    file.WriteLine(BlackListedWord);
+                }
+                await ctx.Message.RespondAsync($"{BlackListedWord} has been added to the blacklist");
+            }
+
+            [Command("RemoveWord")]
+            [Aliases("removeWord", "remove", "Remove")]
+            public async Task RemoveWord(CommandContext ctx, string BlackListedWord)
+            {
+                UserUpdate(ctx, BlackListedWord);
+                await ctx.Message.RespondAsync($"{BlackListedWord} has been removed");
+            }
+
+            [Command("ViewWords")]
+            [Aliases("viewWords", "View", "view")]
+            public async Task ViewWords(CommandContext ctx)
+            {
+                string SingleLineRow = "";
+                using (StreamReader sr = new StreamReader(Blacklist + "-" + ctx.Guild.Id))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        SingleLineRow += line.Replace(',', ' ') + "\n";
+                    }
+                }
+                await ctx.Message.RespondAsync($"``` {SingleLineRow} ```");
+            }
+
+            [Command("AddToBlacklist")]
+            [Aliases("addtoblacklist", "blacklist", "Blacklist", "BlackList")]
+            public async Task AddToBlacklist(CommandContext ctx)
+            {
+                using (StreamWriter file = new StreamWriter(BlacklistedGuild, true))
+                {
+                    file.WriteLine(ctx.Guild.Id + "," + ctx.Guild.Name);
+                }
+                await ctx.Message.RespondAsync("Your guild has been added to the blacklist");
+            }
         }
 
         [Command("Invite")]
